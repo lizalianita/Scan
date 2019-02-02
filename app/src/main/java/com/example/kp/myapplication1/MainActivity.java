f4d442ef398673f7c6fedcc10f84799ede587ee2
lizalianita <44265100+lizalianita@users.noreply.github.com>
2019-02-02 23:05:50 +0700

    IMG

--- app/src/main/java/com/example/kp/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/kp/myapplication1/MainActivity.java
@@ -1,14 +1,16 @@
 package com.example.kp.myapplication1;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 }
-vd
